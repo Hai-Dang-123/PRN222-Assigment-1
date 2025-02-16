b11ee9d82d92672c44dd9e0fcfd1d090d3e9382d
Hai-Dang-123 <danglvhse151369@fpt.edu.vn>
2025-02-17 01:52:25 +0700

    first commit

---
+++ GroupBoizDAL/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GroupBoizDAL.Entities
+{
+    public class Category
+    {
+        [Key]
+        public short CategoryID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string CategoryDesciption { get; set; }
+
+        public short? ParentCategoryID { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        [ForeignKey("ParentCategoryID")]
+        public virtual Category ParentCategory { get; set; }
+
+        public virtual ICollection<Category> SubCategories { get; set; } = new List<Category>();
+        public virtual ICollection<NewsArticle> NewsArticles { get; set; } = new List<NewsArticle>();
+    }
+}
---
+++ GroupBoizDAL/Entities/SystemAccount.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupBoizDAL.Entities
+{
+    public class SystemAccount
+    {
+        [Key]
+        public short AccountID { get; set; }
+
+        [StringLength(100)]
+        public string AccountName { get; set; }
+
+        [StringLength(70)]
+        public string AccountEmail { get; set; }
+
+        public int? AccountRole { get; set; }
+
+        [StringLength(70)]
+        public string AccountPassword { get; set; }
+
+        public virtual ICollection<NewsArticle> CreatedNewsArticles { get; set; } = new List<NewsArticle>();
+        public virtual ICollection<NewsArticle> UpdatedNewsArticles { get; set; } = new List<NewsArticle>();
+    }
+}
